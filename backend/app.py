0bf7ac725da1c5e68ba9439928e7c628488a91b9
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2025-03-10 08:18:40 +0530

    Add admin login and registration functionality with JWT token management

--- backend/app.py
+++ backend/app.py
@@ -15,9 +15,12 @@ from Routes.userConsole.loginRoutes import loginRegister
 from Routes.DevConsole.devLoginRegister import  devLoginRegister
 from Routes.DevConsole.devRoutes import  devRoutes
 
+from Routes.AdminConsole.adminLoginRegister import adminLoginRegister
+
 
 
 # register all blueprints
 app.register_blueprint(loginRegister,url_prefix='/auth')
 app.register_blueprint(devLoginRegister,url_prefix='/dev')
 app.register_blueprint(devRoutes,url_prefix='/devRoutes')
+app.register_blueprint(adminLoginRegister,url_prefix='/admin')
